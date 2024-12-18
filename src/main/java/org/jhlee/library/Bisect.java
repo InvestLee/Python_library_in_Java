c6494fb3f3ec0b6c444dd1aead58e24f76538193
InvestLee <dlwlgud216@nate.com>
2024-12-18 21:24:58 +0900

    bisect library 추가

--- src/main/java/org/jhlee/library/Bisect.java
+++ src/main/java/org/jhlee/library/Bisect.java
@@ -1,4 +1,235 @@
 package org.jhlee.library;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bisect {
+    public static<T> int bisectLeft(List<T> arr, T x, int low, int high){
+        if(low < 0){
+            throw new IllegalArgumentException("The low value must be greater than or equal to 0.");
+        }
+        if(high > arr.size()){
+            throw new IllegalArgumentException("The high value must be less than or equal to the arr length.");
+        }
+
+        boolean flag = false;
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(x instanceof Integer) {
+                flag = (Integer) arr.get(mid) < (Integer) x;
+            } else if(x instanceof Double) {
+                flag = (Double) arr.get(mid) < (Double) x;
+            } else if(x instanceof Float) {
+                flag = (Float) arr.get(mid) < (Float) x;
+            } else if(x instanceof Long) {
+                flag = (Long) arr.get(mid) < (Long) x;
+            } else if(x instanceof Byte) {
+                flag = (Byte) arr.get(mid) < (Byte) x;
+            } else if(x instanceof Short) {
+                flag = (Short) arr.get(mid) < (Short) x;
+            } else if(x instanceof BigDecimal) {
+                flag = new BigDecimal(arr.get(mid).toString()).compareTo((BigDecimal) x) < 0;
+            } else if(x instanceof String){
+                flag = arr.get(mid).toString().compareTo(x.toString()) < 0;
+            } else if(x instanceof Character) {
+                flag = (Character) arr.get(mid) < (Character) x;
+            }
+
+            if(flag){
+                low = mid +1;
+            }
+            else{
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    public static<T> int bisectLeft(List<T> arr, T x){
+        int low = 0;
+        int high = arr.size();
+
+        boolean flag = false;
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(x instanceof Integer) {
+                flag = (Integer) arr.get(mid) < (Integer) x;
+            } else if(x instanceof Double) {
+                flag = (Double) arr.get(mid) < (Double) x;
+            } else if(x instanceof Float) {
+                flag = (Float) arr.get(mid) < (Float) x;
+            } else if(x instanceof Long) {
+                flag = (Long) arr.get(mid) < (Long) x;
+            } else if(x instanceof Byte) {
+                flag = (Byte) arr.get(mid) < (Byte) x;
+            } else if(x instanceof Short) {
+                flag = (Short) arr.get(mid) < (Short) x;
+            } else if(x instanceof BigDecimal) {
+                flag = new BigDecimal(arr.get(mid).toString()).compareTo((BigDecimal) x) < 0;
+            } else if(x instanceof String){
+                flag = arr.get(mid).toString().compareTo(x.toString()) < 0;
+            } else if(x instanceof Character) {
+                flag = (Character) arr.get(mid) < (Character) x;
+            }
+
+            if(flag){
+                low = mid +1;
+            }
+            else{
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    public static<T> int bisectRight(List<T> arr, T x, int low, int high){
+        if(low < 0){
+            throw new IllegalArgumentException("The low value must be greater than or equal to 0.");
+        }
+        if(high > arr.size()){
+            throw new IllegalArgumentException("The high value must be less than or equal to the arr length.");
+        }
+
+        boolean flag = false;
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(x instanceof Integer) {
+                flag = (Integer) arr.get(mid) > (Integer) x;
+            } else if(x instanceof Double) {
+                flag = (Double) arr.get(mid) > (Double) x;
+            } else if(x instanceof Float) {
+                flag = (Float) arr.get(mid) > (Float) x;
+            } else if(x instanceof Long) {
+                flag = (Long) arr.get(mid) > (Long) x;
+            } else if(x instanceof Byte) {
+                flag = (Byte) arr.get(mid) > (Byte) x;
+            } else if(x instanceof Short) {
+                flag = (Short) arr.get(mid) > (Short) x;
+            } else if(x instanceof BigDecimal) {
+                flag = new BigDecimal(arr.get(mid).toString()).compareTo((BigDecimal) x) > 0;
+            } else if(x instanceof String){
+                flag = arr.get(mid).toString().compareTo(x.toString()) > 0;
+            } else if(x instanceof Character) {
+                flag = (Character) arr.get(mid) > (Character) x;
+            }
+
+            if(flag){
+                high = mid;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+
+        return low;
+    }
+
+    public static<T> int bisectRight(List<T> arr, T x){
+        int low = 0;
+        int high = arr.size();
+
+        boolean flag = false;
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(x instanceof Integer) {
+                flag = (Integer) arr.get(mid) > (Integer) x;
+            } else if(x instanceof Double) {
+                flag = (Double) arr.get(mid) > (Double) x;
+            } else if(x instanceof Float) {
+                flag = (Float) arr.get(mid) > (Float) x;
+            } else if(x instanceof Long) {
+                flag = (Long) arr.get(mid) > (Long) x;
+            } else if(x instanceof Byte) {
+                flag = (Byte) arr.get(mid) > (Byte) x;
+            } else if(x instanceof Short) {
+                flag = (Short) arr.get(mid) > (Short) x;
+            } else if(x instanceof BigDecimal) {
+                flag = new BigDecimal(arr.get(mid).toString()).compareTo((BigDecimal) x) > 0;
+            } else if(x instanceof String){
+                flag = arr.get(mid).toString().compareTo(x.toString()) > 0;
+            } else if(x instanceof Character) {
+                flag = (Character) arr.get(mid) > (Character) x;
+            }
+
+            if(flag){
+                high = mid;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+
+        return low;
+    }
+
+    public static<T> List<T> insortLeft(List<T> arr, T x, int low, int high){
+        if(low < 0){
+            throw new IllegalArgumentException("The low value must be greater than or equal to 0.");
+        }
+        if(high > arr.size()){
+            throw new IllegalArgumentException("The high value must be less than or equal to the arr length.");
+        }
+        List<T> result = new ArrayList<>();
+        int insertIndex = bisectLeft(arr,x,low,high);
+        for(int i = 0; i < insertIndex; i++){
+            result.add(arr.get(i));
+        }
+        result.add(x);
+        for(int i = insertIndex; i < arr.size(); i++){
+            result.add(arr.get(i));
+        }
+        return result;
+    }
+
+    public static<T> List<T> insortLeft(List<T> arr, T x){
+        int low = 0;
+        int high = arr.size();
+        List<T> result = new ArrayList<>();
+        int insertIndex = bisectLeft(arr,x);
+        for(int i = 0; i < insertIndex; i++){
+            result.add(arr.get(i));
+        }
+        result.add(x);
+        for(int i = insertIndex; i < arr.size(); i++){
+            result.add(arr.get(i));
+        }
+        return result;
+    }
+
+    public static<T> List<T> insortRight(List<T> arr, T x, int low, int high){
+        if(low < 0){
+            throw new IllegalArgumentException("The low value must be greater than or equal to 0.");
+        }
+        if(high > arr.size()){
+            throw new IllegalArgumentException("The high value must be less than or equal to the arr length.");
+        }
+        List<T> result = new ArrayList<>();
+        int insertIndex = bisectRight(arr,x,low,high);
+        for(int i = 0; i < insertIndex; i++){
+            result.add(arr.get(i));
+        }
+        result.add(x);
+        for(int i = insertIndex; i < arr.size(); i++){
+            result.add(arr.get(i));
+        }
+        return result;
+    }
+
+    public static<T> List<T> insortRight(List<T> arr, T x){
+        int low = 0;
+        int high = arr.size();
+        List<T> result = new ArrayList<>();
+        int insertIndex = bisectRight(arr,x);
+        for(int i = 0; i < insertIndex; i++){
+            result.add(arr.get(i));
+        }
+        result.add(x);
+        for(int i = insertIndex; i < arr.size(); i++){
+            result.add(arr.get(i));
+        }
+        return result;
+    }
 }
---
+++ src/test/java/org/jhlee/library/BisectTest.java
@@ -0,0 +1,79 @@
+package org.jhlee.library;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BisectTest {
+
+    @Test
+    @DisplayName("bisectLeft 함수")
+    public void bisectLeftTest() {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80, 90, 100));
+        assertEquals(0, Bisect.bisectLeft(arr,0));
+        assertEquals(2, Bisect.bisectLeft(arr,25));
+        assertEquals(4, Bisect.bisectLeft(arr,50));
+        assertEquals(7, Bisect.bisectLeft(arr,75));
+        assertEquals(9, Bisect.bisectLeft(arr,100));
+        assertEquals(10, Bisect.bisectLeft(arr,125));
+
+        assertEquals(2, Bisect.bisectLeft(arr,0,2,8));
+        assertEquals(4, Bisect.bisectLeft(arr,50,2,8));
+        assertEquals(8, Bisect.bisectLeft(arr,100,2,8));
+    }
+
+    @Test
+    @DisplayName("bisectRight 함수")
+    public void bisectRightTest() {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80, 90, 100));
+        assertEquals(0, Bisect.bisectRight(arr,0));
+        assertEquals(2, Bisect.bisectRight(arr,25));
+        assertEquals(5, Bisect.bisectRight(arr,50));
+        assertEquals(7, Bisect.bisectRight(arr,75));
+        assertEquals(10, Bisect.bisectRight(arr,100));
+        assertEquals(10, Bisect.bisectRight(arr,125));
+
+        assertEquals(2, Bisect.bisectRight(arr,0,2,8));
+        assertEquals(5, Bisect.bisectRight(arr,50,2,8));
+        assertEquals(8, Bisect.bisectRight(arr,100,2,8));
+    }
+
+    @Test
+    @DisplayName("insortLeft 함수")
+    public void insortLeftTest() {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+        assertEquals(Arrays.asList(0, 10, 20, 30, 40, 50), Bisect.insortLeft(arr,0));
+        assertEquals(Arrays.asList(10, 15, 20, 30, 40, 50), Bisect.insortLeft(arr,15));
+        assertEquals(Arrays.asList(10, 20, 30, 30, 40, 50), Bisect.insortLeft(arr,30));
+        assertEquals(Arrays.asList(10, 20, 30, 40, 50, 50), Bisect.insortLeft(arr,50));
+        assertEquals(Arrays.asList(10, 20, 30, 40, 50, 100), Bisect.insortLeft(arr,100));
+
+        assertEquals(Arrays.asList(10, 0, 20, 30, 40, 50), Bisect.insortLeft(arr,0,1,4));
+        assertEquals(Arrays.asList(10, 15, 20, 30, 40, 50), Bisect.insortLeft(arr,15,1,4));
+        assertEquals(Arrays.asList(10, 20, 30, 30, 40, 50), Bisect.insortLeft(arr,30,1,4));
+        assertEquals(Arrays.asList(10, 20, 30, 40, 50, 50), Bisect.insortLeft(arr,50,1,4));
+        assertEquals(Arrays.asList(10, 20, 30, 40, 100, 50), Bisect.insortLeft(arr,100,1,4));
+    }
+
+    @Test
+    @DisplayName("insortRight 함수")
+    public void insortRightTest() {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+        assertEquals(Arrays.asList(0, 10, 20, 30, 40, 50), Bisect.insortRight(arr,0));
+        assertEquals(Arrays.asList(10, 15, 20, 30, 40, 50), Bisect.insortRight(arr,15));
+        assertEquals(Arrays.asList(10, 20, 30, 30, 40, 50), Bisect.insortRight(arr,30));
+        assertEquals(Arrays.asList(10, 20, 30, 40, 50, 50), Bisect.insortRight(arr,50));
+        assertEquals(Arrays.asList(10, 20, 30, 40, 50, 100), Bisect.insortRight(arr,100));
+
+        assertEquals(Arrays.asList(10, 0, 20, 30, 40, 50), Bisect.insortRight(arr,0,1,4));
+        assertEquals(Arrays.asList(10, 15, 20, 30, 40, 50), Bisect.insortRight(arr,15,1,4));
+        assertEquals(Arrays.asList(10, 20, 30, 30, 40, 50), Bisect.insortRight(arr,30,1,4));
+        assertEquals(Arrays.asList(10, 20, 30, 40, 50, 50), Bisect.insortRight(arr,50,1,4));
+        assertEquals(Arrays.asList(10, 20, 30, 40, 100, 50), Bisect.insortRight(arr,100,1,4));
+    }
+}
